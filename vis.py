6f66212a68cc647541dec1e233c0d3f317f1be27
Kyle Laker <lakerka@dukes.jmu.edu>
2017-04-25 17:10:13 -0400

    vis.py: Rename variables to make more sense

--- vis.py
+++ vis.py
@@ -16,12 +16,11 @@ def visdot():
 
 # visualize the elliptic curve used
 def viscurve(a, b):
-    Y, X = np.ogrid[-5:300:100j, -5:300:100j]
+    y, x = np.ogrid[-5:300:100j, -5:300:100j]
     # y^2 = x^3 + Ax + B ==> y^2 - x^3 - Ax - B
-    plt.contour(X.ravel(), Y.ravel(), pow(Y, 2) - pow(X, 3) - X * a - b, [0])
+    plt.contour(x.ravel(), y.ravel(), pow(y, 2) - pow(x, 3) - x * a - b, [0])
     plt.grid()
     return None
-
 
 
 def main():
@@ -38,64 +37,64 @@ def main():
 
     # set arguments
     if len(sys.argv) == 3:
-        cfile = sys.argv[2]
+        coord_file = sys.argv[2]
     else:
         if os.path.isfile("coords.csv"):
-            cfile = "coords.csv"
+            coord_file = "coords.csv"
         else:
             print("Please provide a .csv file containing coords")
             sys.exit()
 
     if len(sys.argv) == 4:
-        dfile = sys.argv[3]
+        data_file = sys.argv[3]
     else:
         if os.path.isfile("data.csv"):
-            dfile = "data.csv"
+            data_file = "data.csv"
         else:
             print("Please provide a .csv file containing an elliptic curve function")
             sys.exit()
 
     # read in coords from csv file
-    with open(cfile, 'r') as coords:
-        creader = csv.reader(coords, delimiter=',')
-        for row in creader:
-            x_coords.append(row[0])
-            y_coords.append(row[1])
+    with open(coord_file, 'r') as coords:
+        coord_reader = csv.reader(coords, delimiter=',')
+        for row in coord_reader:
+            x_coords.append(int(row[0]))
+            y_coords.append(int(row[1]))
     coords.close()
 
     # read in other data from data csv
     # data format: A, B, base point, alice pub key, bob pub key, data x, data y
     #               encrypted x, encrypted y
-    with open(dfile, 'r') as data:
-        dreader = csv.reader(data, delimiter=',')
+    with open(data_file, 'r') as data:
+        data_reader = csv.reader(data, delimiter=',')
 
         # Get curve function a and b
-        ab = next(dreader)
+        ab = next(data_reader)
         a = int(ab[0])
         b = int(ab[1])
 
         # Get base point of curve (important for keys)
-        bp = next(dreader)
+        bp = next(data_reader)
         bpx = int(bp[0][1:])
         bpy = int(bp[1][1:-1])
 
         # Get Alice's public key
-        ak = next(dreader)
+        ak = next(data_reader)
         akx = int(ak[0][1:])
         aky = int(ak[1][1:-1])
 
         # Get Bob's public key
-        bk = next(dreader)
+        bk = next(data_reader)
         bkx = int(bk[0][1:])
         bky = int(bk[1][1:-1])
 
-        # Get cleartext data
-        clt = next(dreader)
+        # Get clear text data
+        clt = next(data_reader)
         cltx = clt[0]
         clty = clt[1]
 
-        # Get cyphertext data
-        cyt = next(dreader)
+        # Get cypher text data
+        cyt = next(data_reader)
         cytx = cyt[0]
         cyty = cyt[1]
 
@@ -119,7 +118,7 @@ def main():
     print("Red - Encrypted data")
 
     # does not work - output is weird
-    viscurve(a, b)
+    # viscurve(a, b)
 
     # display output plot
     plt.show()
